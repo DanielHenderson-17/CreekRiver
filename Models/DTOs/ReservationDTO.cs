f9f1f9c90f63dcf081e05e8abddfc36cfdc5e600
Daniel Henderson <djhindu17@gmail.com>
2024-11-20 21:51:28 -0600

    CreekRiver finished

--- Models/DTOs/ReservationDTO.cs
+++ Models/DTOs/ReservationDTO.cs
@@ -5,7 +5,21 @@ public class ReservationDTO
     public int UserProfileId { get; set; }
     public DateTime CheckinDate { get; set; }
     public DateTime CheckoutDate { get; set; }
+
+    // Calculated property for the total number of nights
+    public int TotalNights => (CheckoutDate - CheckinDate).Days;
+
+    private static readonly decimal _reservationBaseFee = 10M;
+
+    // Calculated property for the total cost
+    public decimal TotalCost
+    {
+        get
+        {
+            return Campsite.CampsiteType.FeePerNight * TotalNights + _reservationBaseFee;
+        }
+    }
+
     public UserProfileDTO UserProfile { get; set; }
     public CampsiteDTO Campsite { get; set; }
 }
-
